e2860f606430a8282345b53b7aa1977c967aef28
hemanth1718262 <garrehemanth9@gmail.com>
2025-01-10 21:52:55 +0530

    Add files via upload

---
+++ 217/Demoo6.java
@@ -0,0 +1,10 @@
+public class Demoo6 {
+    public static void main(String[] args) {
+        String username="bhavya";
+        String psw = "12345";
+        while((username == "bhavya") && (psw == "12345")){
+            System.out.println("welcome to mallareddyuniversity");
+        }
+    }
+    
+}
