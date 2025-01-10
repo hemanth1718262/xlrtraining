d326f9e4e80e713cc7f9e8b6f304fe8b1d676658
hemanth1718262 <garrehemanth9@gmail.com>
2025-01-10 21:57:34 +0530

    Add files via upload

---
+++ 217/Demo16.java
@@ -0,0 +1,14 @@
+public class Demo16 {
+    class PalindromeOf3 {
+        boolean isPalindromeOf3Digits(int n){
+            return (n/ 100) == (n % 10);
+        }
+    }
+    public class Demo016 {
+        public static void main(String[] args) {
+            int n = 131;	
+            System.out.println(obj.isPalindromeOf3Digits(n));
+        }
+    }
+    
+}
