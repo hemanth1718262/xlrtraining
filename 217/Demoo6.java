b8112a42f3cefc09249a2d7cbde95b2f187a7e6d
hemanth1718262 <garrehemanth9@gmail.com>
2025-01-10 21:53:33 +0530

    Update Demoo6.java

--- 217/Demoo6.java
+++ 217/Demoo6.java
@@ -2,7 +2,7 @@ public class Demoo6 {
     public static void main(String[] args) {
         String username="bhavya";
         String psw = "12345";
-        while((username == "bhavya") && (psw == "12345")){
+        while((username == "hemanth") && (psw == "12345")){
             System.out.println("welcome to mallareddyuniversity");
         }
     }
