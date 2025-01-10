e15c159556f195b42b7b51d291e87bdfa226aaab
hemanth1718262 <garrehemanth9@gmail.com>
2025-01-10 21:54:35 +0530

    Add files via upload

---
+++ 217/Demo12.java
@@ -0,0 +1,20 @@
+public class  Demo12{
+
+   
+       void Area() {
+           
+           int r = 5;
+           
+           double ca = Math.PI* Math.pow(r, 2);
+           
+           System.out.println("The circle area is : " + ca);
+       }
+   
+   }
+   public class Demo012 {
+       public static void main(String[] args) {
+           CircleAreaDemo12 obj = new CircleAreaDemo12();
+           obj.Area();
+   }
+    
+}
