e640dfb1e927755076f881147d055ea6d57bf447
subhash <garrehemanth9@gmail.com>
2025-01-26 20:00:53 +0530

    Add files via upload

---
+++ 217/Demo042.java
@@ -0,0 +1,12 @@
+public class Demo042 {
+
+    public static void main(String[] args) {
+        
+        char lowercaseChar = 'b';
+
+    
+        char uppercaseChar = Character.toUpperCase(lowercaseChar);
+        System.out.println("Lowercase character: " + lowercaseChar);
+        System.out.println("Uppercase character: " + uppercaseChar);
+    }
+}
