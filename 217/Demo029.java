980522d4ad457bf0cadaf1bdd1a6e6e5e8e13532
subhash <garrehemanth9@gmail.com>
2025-01-26 19:54:47 +0530

    Add files via upload

---
+++ 217/Demo029.java
@@ -0,0 +1,10 @@
+public class Demo029 {
+    public static void main(String[] args) {
+        String[] friends = {"Bhavya" , "Lucky" , "Lally"};
+
+        System.out.println("List of Friends:");
+        for (String friend : friends) {
+            System.out.println(friend);
+        }
+    }
+}
